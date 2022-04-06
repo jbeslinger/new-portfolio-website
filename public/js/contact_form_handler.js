659218634c5954b9e645161d9b4d0c3f0bcfe11f
jbeslinger <jbeslinger@gmail.com>
2022-04-06 19:54:31 -0400

    Nodemailer functionality implemented.

---
+++ public/js/contact_form_handler.js
@@ -0,0 +1,36 @@
+const contactForm = document.getElementById('mailMe');
+let senderName = document.getElementById('senderName');
+let emailAddress = document.getElementById('emailAddress');
+let subjectLine = document.getElementById('subjectLine');
+let emailBody = document.getElementById('emailBody');
+
+const msgSuccessfullySent = "Email delivered! I'll reach out to you soon. Thanks!";
+const msgFailedToSend = "Sorry; an error has occurred and your email has not been sent.";
+
+contactForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    let formData = {
+        senderName: senderName.value,
+        emailAddress: emailAddress.value,
+        subjectLine: subjectLine.value,
+        emailBody: emailBody.value
+    };
+
+    let xhr = new XMLHttpRequest();
+    xhr.open('post', '/send');
+    xhr.setRequestHeader('content-type', 'application/json');
+    xhr.onload = () => {
+        if (xhr.responseText == 'success') {
+            alert(msgSuccessfullySent);
+            senderName.value = '';
+            emailAddress.value = '';
+            subjectLine.value = '';
+            emailBody.value = '';
+        } else {
+            alert(msgFailedToSend);
+        }
+    };
+
+    xhr.send(JSON.stringify(formData));
+});
--- server.js
+++ server.js
@@ -4,8 +4,13 @@ const PORT = process.env.PORT || 3000;
 
 const mongoose = require('mongoose');
 const dbConfig = require('./config/database.js');
-const app = express();
 
+const app = express();
+const bodyParser = require('body-parser')
+const jsonParser = bodyParser.json();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 
@@ -14,22 +19,29 @@ const portfolios = require('./models/queries/get_portfolios');
 const articles = require('./models/queries/get_articles');
 const navBar = require('./config/navbar');
 
+const mailer = require('./config/nodemailer');
+
 // database configuration
 mongoose.connect(dbConfig.url);
-
-// we only have one route - DO WE NEED THE ARRAY OF ROUTES? 
-// we can have additional routes for each of the portofolios if you wish it to be that way
-// but I was thinking that the only other routes that were going to be there would be 
-// for the blog - articles
-const routes = ['/', '/intro', '/about', '/skills', '/portfolio', '/contact']
 
 app.get('/', async (req, res) =>  {
     const allSkills = await skills.getAllSkills()
     const allPortfolios = await portfolios.getAllPortfolio();
-    const allArticles =  await articles.getAllArticles();
+    const allArticles = await articles.getAllArticles();
 
     res.render('layout', { navBar: navBar.getnavBarItems, skills : allSkills , blog: allArticles , portfolio : allPortfolios});
 });
+
+app.post('/send', jsonParser, async (req, res) => {
+    var error = await mailer.sendEmail(req.body);
+    if (error) {
+        console.log("Error: " + error);
+        res.send('error');
+    } else {
+        console.log("New message sent successfully.");
+        res.send('success');
+    }
+})
 
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
